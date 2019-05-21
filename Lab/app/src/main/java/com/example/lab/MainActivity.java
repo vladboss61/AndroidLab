958b2042cce6c4681aa1ad919b66d5d73a396076
Varvashenko Vlad <varvashenko19972017@gmail.com>
2019-05-21 21:55:51 +0300

    Lab 2

--- Lab/app/src/main/java/com/example/lab/MainActivity.java
+++ Lab/app/src/main/java/com/example/lab/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.lab;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView _timerView;
+    private CountDownTimer _countDownTimer;
     private Button _next;
     private Button _previous;
 
@@ -15,12 +19,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialization();
+        startTimer();
         addEvents();
     }
 
     private void initialization(){
+        this._timerView = (TextView)findViewById(R.id.timer);
         this._next = (Button)findViewById(R.id.next_button);
         this._previous = (Button)findViewById(R.id.previous_button);
+    }
+
+    private void startTimer(){
+        this._countDownTimer = new CountDownTimer(600000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                _timerView.setText(
+                        millisUntilFinished
+                        + " : "
+                        + millisUntilFinished % 600000 / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
     }
 
     private void addEvents(){
